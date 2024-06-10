29d1b57db4f7bdc535d48c794107c1ef173e08a4
sowjanyakch <sowjanya.kch@gmail.com>
2024-06-10 22:57:17 +0200

    TopAppBar
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityCompose.kt
+++ app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityCompose.kt
@@ -10,15 +10,72 @@ package com.nextcloud.talk.contacts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import autodagger.AutoInjector
+import com.nextcloud.talk.R
+import com.nextcloud.talk.api.NcApi
+import com.nextcloud.talk.application.NextcloudTalkApplication
+import com.nextcloud.talk.users.UserManager
+import javax.inject.Inject
+
+@AutoInjector(NextcloudTalkApplication::class)
 class ContactsActivityCompose : ComponentActivity() {
+
+    @Inject
+    lateinit var userManager: UserManager
+
+    @Inject
+    lateinit var ncApi: NcApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        NextcloudTalkApplication.sharedApplication!!.componentApplication.inject(this)
         setContent {
-            TextMessage()
+            MaterialTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = stringResource(R.string.nc_app_product_name), onBackClick = {
+                            onBackPressedDispatcher.onBackPressed()
+                            // not working currently with compose
+                        })
+                    },
+                    content = {
+                    }
+                )
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(title: String, onBackClick: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = {
+                onBackClick
+            }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* Handle search action */ }) {
+                Icon(Icons.Filled.Search, contentDescription = "Search")
+            }
+        }
+    )
 }
 
 @Composable
