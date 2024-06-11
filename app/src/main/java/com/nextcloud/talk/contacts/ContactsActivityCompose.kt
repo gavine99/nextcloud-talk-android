6eb5985596074568433b9d64c7306153dfbea986
sowjanyakch <sowjanya.kch@gmail.com>
2024-06-11 23:18:21 +0200

    Conversation creation options
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityCompose.kt
+++ app/src/main/java/com/nextcloud/talk/contacts/ContactsActivityCompose.kt
@@ -10,8 +10,12 @@ package com.nextcloud.talk.contacts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,7 +25,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import autodagger.AutoInjector
 import com.nextcloud.talk.R
 import com.nextcloud.talk.api.NcApi
@@ -51,6 +58,9 @@ class ContactsActivityCompose : ComponentActivity() {
                         })
                     },
                     content = {
+                        Column(Modifier.padding(it)) {
+                            ConversationCreationOptions()
+                        }
                     }
                 )
             }
@@ -67,7 +77,7 @@ fun TopAppBar(title: String, onBackClick: () -> Unit) {
             IconButton(onClick = {
                 onBackClick
             }) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
             }
         },
         actions = {
@@ -79,6 +89,21 @@ fun TopAppBar(title: String, onBackClick: () -> Unit) {
 }
 
 @Composable
-fun TextMessage() {
-    Text("This is a test message")
+fun ConversationCreationOptions() {
+    Column(modifier = Modifier.padding(10.dp)) {
+        Row(modifier = Modifier.padding(10.dp)) {
+            Image(
+                painter = painterResource(R.drawable.baseline_chat_bubble_outline_24),
+                contentDescription = "New Conversation Creation Icon"
+            )
+            Text(text = stringResource(R.string.nc_create_new_conversation))
+        }
+        Row(modifier = Modifier.padding(10.dp)) {
+            Image(
+                painter = painterResource(R.drawable.baseline_format_list_bulleted_24),
+                contentDescription = "Join open conversations Icon"
+            )
+            Text(text = stringResource(R.string.nc_join_open_conversations))
+        }
+    }
 }
